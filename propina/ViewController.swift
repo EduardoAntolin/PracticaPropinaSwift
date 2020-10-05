f17b2340d7dfdc10742f7cd7738fd4d397510235
Alumno <ulsa@d104.local>
2020-10-05 13:22:00 -0700

    Initial Commit

---
+++ propina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  propina
+//
+//  Created by Alumno on 10/5/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
